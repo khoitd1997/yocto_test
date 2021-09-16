c4ef6e3e126e6b812edbe79df662dfcb7f037797
khoitd1997 <khoidinhtrinh@gmail.com>
2021-09-16 10:33:44 -0700

    added stuffs

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 set -e
 
 script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
@@ -9,4 +11,5 @@ source oe-init-build-env
 cp ${custom_conf_dir}/local.conf ${poky_build_conf_dir}
 cp ${custom_conf_dir}/bblayers.conf ${poky_build_conf_dir}
 
-bitbake core-image-minimal
+# bitbake core-image-minimal
+bitbake core-image-sato
--- unpack_sstate.sh
+++ unpack_sstate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 source ${script_dir}/common.sh
