51b66a149fbb68574fcd7c84a34a5adc33782875
khoitd1997 <khoidinhtrinh@gmail.com>
2021-09-21 22:10:08 -0700

    Added clean_all script

--- clean_all.sh
+++ clean_all.sh
@@ -5,4 +5,4 @@ set -e
 script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source ${script_dir}/common.sh
 
-rm -rf ${poky_build_dir}
+rm -rf ${poky_build_dir} ${bitbake_cache_dir} ${sstate_dir} ${dl_dir}
