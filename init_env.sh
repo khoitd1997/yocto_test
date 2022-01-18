07b6b8be50b907f6383c256b4f30b488c8c1d046
khoitd1997 <khoidinhtrinh@gmail.com>
2022-01-18 09:53:47 -0800

    conf file refactor

--- init_env.sh
+++ init_env.sh
@@ -3,6 +3,6 @@
 # into the current shell
 
 script_dir=$(dirname $(readlink -f ${BASH_SOURCE}))
-source ${script_dir}/common.sh
+source "${script_dir}/common.sh"
 
 source_oe_init_script
--- set_build_config.sh
+++ set_build_config.sh
@@ -6,7 +6,7 @@
 set -e
 
 script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-source ${script_dir}/common.sh
+source "${script_dir}/common.sh"
 
 build_config="${1}"
 if [ -z "${build_config}" ]; then
