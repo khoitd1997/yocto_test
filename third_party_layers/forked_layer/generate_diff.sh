af182c85c106f6c303f2cdc7983a21387e3da0d5
khoitd1997 <khoidinhtrinh@gmail.com>
2022-02-06 23:08:32 -0800

    updated Xilinx layers

--- third_party_layers/forked_layer/generate_diff.sh
+++ third_party_layers/forked_layer/generate_diff.sh
@@ -9,11 +9,14 @@ mkdir "${diff_out_dir}"
 cd /tmp
 rm -rf meta-openamp meta-xilinx-tools meta-xilinx
 
-git clone https://github.com/Xilinx/meta-openamp.git -b xlnx-rel-v2021.2
+git clone https://github.com/Xilinx/meta-openamp.git
+cd meta-openamp && git checkout 670161c5d246ba6af293e274b7fd5b34f9a44701 && cd /tmp
 diff -bur --exclude=.git "${script_dir}/meta-openamp-xlnx-rel-v2021.2" meta-openamp > "${diff_out_dir}/meta-openamp-diff.patch"
 
-git clone https://github.com/Xilinx/meta-xilinx.git -b xlnx-rel-v2021.2
+git clone https://github.com/Xilinx/meta-xilinx.git
+cd meta-xilinx && git checkout 054cb2cb4c3ffe868ade68369433027989571cf6 && cd /tmp
 diff -bur --exclude=.git "${script_dir}/meta-xilinx-xlnx-rel-v2021.2" meta-xilinx > "${diff_out_dir}/meta-xilinx-diff.patch"
 
-git clone https://github.com/Xilinx/meta-xilinx-tools.git -b xlnx-rel-v2021.2
+git clone https://github.com/Xilinx/meta-xilinx-tools.git
+cd meta-xilinx-tools && git checkout 9be31b43df41ec245f969c2fc65f56c6e910d752 && cd /tmp
 diff -bur --exclude=.git "${script_dir}/meta-xilinx-tools-xlnx-rel-v2021.2" meta-xilinx-tools > "${diff_out_dir}/meta-xilinx-tools-diff.patch"
