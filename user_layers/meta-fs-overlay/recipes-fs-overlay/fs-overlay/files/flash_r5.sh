5b66f55810cdbfbc22cd000ce1bd28b843781133
Khoi Trinh <khoidinhtrinh@gmail.com>
2022-02-25 08:47:07 -0800

    fixed r5 flash script

--- user_layers/meta-fs-overlay/recipes-fs-overlay/fs-overlay/files/flash_r5.sh
+++ user_layers/meta-fs-overlay/recipes-fs-overlay/fs-overlay/files/flash_r5.sh
@@ -2,10 +2,14 @@
 
 set -e
 
+mkdir -p "/lib/firmware"
+
 r5_image_path="${1}"
 r5_image_name=$(basename "${r5_image_path}")
 
+set +e
 echo stop > /sys/class/remoteproc/remoteproc0/state
+set -e
 
 cp "${r5_image_path}" "/lib/firmware"
 
